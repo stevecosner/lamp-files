8d5aa7212bccc23f6cd72307860adfa1f70364da
stevecosner <48138889+stevecosner@users.noreply.github.com>
2019-12-07 16:30:10 -0500

    Update my.php

--- my.php
+++ my.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "newuser";
-$password = "cleartext_password";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
